003a20e3caa3c5e0ceb3a1667d4b65913f5d0608
HannaMalygina <gannamalygina@gmail.com>
2024-02-22 17:50:11 +0100

    working version get_numbers_tickets

---
+++ get_numbers_ticket.py
@@ -0,0 +1,23 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    
+    if min < 1: 
+        print("first argument of the function is 'min' and must be > 0")
+        return ""
+    if max < min:
+        print("second argument of the function is 'max' and must be bigger than the first argument 'min'")
+        return ""
+    if max > 1000:
+        print("second argument of the function is 'max' and must be <= 1000")
+        return ""
+    if quantity > max or quantity < min:
+        print("third argument of the function is 'quantity' and must be min < quantity < max ")
+        return ""
+
+    result = set()
+    while len(result) < quantity:
+        result.add(random.randint(min, max))
+    result_list = list(result)
+    result_list.sort()
+    return result
