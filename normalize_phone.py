606fede86289de6cde53a76b33427888ac4a0c04
HannaMalygina <gannamalygina@gmail.com>
2024-02-27 14:12:24 +0100

    adapted for more countries

--- normalize_phone.py
+++ normalize_phone.py
@@ -2,12 +2,21 @@ import re
 
 def normalize_phone(phone_number):
     phone_number_init = phone_number
+    startsWithPlus = False
+    if phone_number_init[0] == "+": startsWithPlus = True
+
     pattern_remove_symbols = r"\D+"
     replacement = ""
     phone_number_wo_symbols = re.sub(pattern_remove_symbols, replacement, phone_number_init)
-    print (f"{len(phone_number_wo_symbols)}")
-    if len(phone_number_wo_symbols) > 10: 
+    #print (f"{len(phone_number_wo_symbols)}")
+    if startsWithPlus: 
         phone_number_res = "+" + phone_number_wo_symbols
     else:
-        phone_number_res = "+38" + phone_number_wo_symbols
+        if len(phone_number_wo_symbols) > 10: 
+            phone_number_res = "+" + phone_number_wo_symbols
+        else:
+            phone_number_res = "+38" + phone_number_wo_symbols
     return phone_number_res
+
+print(normalize_phone("+49029375755"))
+print(normalize_phone("4902935755"))
