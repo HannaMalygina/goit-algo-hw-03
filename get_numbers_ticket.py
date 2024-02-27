356ec133932f28a7139c26dec222607a19fc3734
HannaMalygina <gannamalygina@gmail.com>
2024-02-27 13:42:08 +0100

    get_numbers_ticket with new requirements

--- get_numbers_ticket.py
+++ get_numbers_ticket.py
@@ -3,16 +3,16 @@ import random
 def get_numbers_ticket(min, max, quantity):
     
     if min < 1: 
-        print("first argument of the function is 'min' and must be > 0")
+        #print("first argument of the function is 'min' and must be > 0")
         return ""
     if max < min:
-        print("second argument of the function is 'max' and must be bigger than the first argument 'min'")
+        #print("second argument of the function is 'max' and must be bigger than the first argument 'min'")
         return ""
     if max > 1000:
-        print("second argument of the function is 'max' and must be <= 1000")
+        #print("second argument of the function is 'max' and must be <= 1000")
         return ""
-    if quantity > max or quantity < min:
-        print("third argument of the function is 'quantity' and must be min < quantity < max ")
+    if quantity > max-min+1 or quantity <= 0:
+        #print("third argument of the function is 'quantity' and must be 0 < quantity <= max-min+1 ")
         return ""
 
     result = set()
@@ -22,3 +22,5 @@ def get_numbers_ticket(min, max, quantity):
     result_list.sort()
     return result
 
+get_numbers_ticket(10,20,12)
+#print (f"get_numbers_ticket returns {x}")
