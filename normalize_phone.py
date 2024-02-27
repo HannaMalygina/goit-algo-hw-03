33c3c332c139612eb816238d53872fa2d887caac
HannaMalygina <gannamalygina@gmail.com>
2024-02-27 14:00:57 +0100

    normilize_phone adapted for international numbers

--- normalize_phone.py
+++ normalize_phone.py
@@ -5,8 +5,8 @@ def normalize_phone(phone_number):
     pattern_remove_symbols = r"\D+"
     replacement = ""
     phone_number_wo_symbols = re.sub(pattern_remove_symbols, replacement, phone_number_init)
-
-    if phone_number_wo_symbols[0]=="3":
+    print (f"{len(phone_number_wo_symbols)}")
+    if len(phone_number_wo_symbols) > 10: 
         phone_number_res = "+" + phone_number_wo_symbols
     else:
         phone_number_res = "+38" + phone_number_wo_symbols
