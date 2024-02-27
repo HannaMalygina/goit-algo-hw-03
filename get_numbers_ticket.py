52e5f21d3532e96ac2daca6f853675c19ff6cca1
HannaMalygina <gannamalygina@gmail.com>
2024-02-27 14:42:42 +0100

    get_numbers_ticket returns empty list

--- get_numbers_ticket.py
+++ get_numbers_ticket.py
@@ -2,25 +2,25 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     
+    result = set()
     if min < 1: 
         #print("first argument of the function is 'min' and must be > 0")
-        return ""
+        return list(result)
     if max < min:
         #print("second argument of the function is 'max' and must be bigger than the first argument 'min'")
-        return ""
+        return list(result)
     if max > 1000:
         #print("second argument of the function is 'max' and must be <= 1000")
-        return ""
+        return list(result)
     if quantity > max-min+1 or quantity <= 0:
         #print("third argument of the function is 'quantity' and must be 0 < quantity <= max-min+1 ")
-        return ""
+        return list(result)
 
-    result = set()
     while len(result) < quantity:
         result.add(random.randint(min, max))
     result_list = list(result)
     result_list.sort()
-    return result
+    return result_list
 
-get_numbers_ticket(10,20,12)
+print(get_numbers_ticket(10,20,10))
 #print (f"get_numbers_ticket returns {x}")
